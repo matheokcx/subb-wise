e6141e4b4fdbc853e5872ab38ce3d7432258943d
Deleplanque Matheo <matheo_deleplanque@ens.univ-artois.fr>
2024-12-26 01:28:38 +0100

    Probleme au niveau de l action pour recup toutes les categories distincts

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,6 +1,7 @@
 import { createClient } from "@/utils/supabase/server";
 import React, { useEffect, useState } from "react";
 import Abonnement from "../composants/Abonnement";
+import Link from "next/link";
 
 export default async function page() {
     const client = await createClient();
@@ -9,8 +10,9 @@ export default async function page() {
     const { data: abonnements } = await client.from('abonnements').select();
 
     return (
-        <>
-            <div>{abonnements?.map((e: any) => <Abonnement e={e} />)}</div>
-        </>
+        <div>
+            <button className='w-1/12 h-12 rounded-xl bg-black text-white font-bold'><Link href='/dashboard/ajouter'>Ajouter</Link></button>
+            {abonnements?.map((e: any) => <Abonnement e={e} />)}
+        </div>
     );
 }
