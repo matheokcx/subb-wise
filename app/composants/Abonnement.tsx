30e6c622629dc8cd5c12b61c2b384fbde68d28eb
Deleplanque Matheo <matheo_deleplanque@ens.univ-artois.fr>
2024-12-26 17:14:16 +0100

    Ajout du champ select de categorie dans le formulaire d ajout d abonnement

--- app/composants/Abonnement.tsx
+++ app/composants/Abonnement.tsx
@@ -1,6 +1,6 @@
 export default function Abonnement({ e }: { e: any }) {
     return (
-        <div className="w-1/6 px-8 py-4 flex flex-col gap-4 justify-center border-2 border-gray-400 rounded-xl">
+        <div key={e.id} className="w-1/6 px-8 py-4 flex flex-col gap-4 justify-center border-2 border-gray-400 rounded-xl">
             <img src={"/" + e.image} alt="Illustration de l'abonnement" />
             <h2 className='text-xl font-bold'>{e.nom}</h2>
             <p>${e.mensualite} | {e.categorie}</p>
--- app/dashboard/ajouter/page.tsx
+++ app/dashboard/ajouter/page.tsx
@@ -1,16 +1,26 @@
-import React from "react";
-import { addAbonne, getCategories } from "./actions";
-import { getCallSite } from "node:util";
+'use client'
 
-export default function page() {
+import { useEffect, useState } from 'react';
+import { getCategories } from './actions';
 
-    getCategories();
+export default function AddSubscriptionPage() {
+    const [categories, setCategories] = useState<string[]>([]);
+
+    useEffect(() => {
+        async function fetchCategories() {
+            const fetchedCategories = await getCategories();
+            setCategories(fetchedCategories || []);
+        }
+
+        fetchCategories();
+    }, []);
 
     return (
-        <>
-            <form action={addAbonne} method='POST'>
-
-            </form>
-        </>
+        <form action="/add-abonnement" method="POST">
+            <select id="categorie" name="categorie">
+                {categories.map((category, index) => <option key={index} value={category} >{category}</option>)}
+            </select>
+            <button type="submit">Ajouter</button>
+        </form>
     );
 }
