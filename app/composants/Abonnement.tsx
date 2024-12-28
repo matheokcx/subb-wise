71ea73e11bcb9100e7f862f35f8cd96f8e8e9893
Deleplanque Matheo <matheo.deleplanque@gmail.com>
2024-12-28 15:12:58 +0100

    Sauvegarde

--- app/composants/Abonnement.tsx
+++ app/composants/Abonnement.tsx
@@ -7,7 +7,7 @@ export default async function Abonnement({ idAbonnement }: { idAbonnement: any }
     const abonnement = abonnements != null ? abonnements[0] : null;
 
     return (
-        <div className="w-11/12 md:w-1/6 px-8 py-4 flex flex-col gap-4 justify-center shadow-lg rounded-3xl transition-all hover:-translate-y-2">
+        <div className="w-11/12 md:w-1/6 px-8 py-4 flex flex-col gap-4 justify-center shadow-lg rounded-3xl transition-all hover:-translate-y-2 hover:shadow-xl">
             <img src={"/" + abonnement.image} alt="Illustration de l'abonnement" className='h-[230px] rounded-lg' />
             <h2 className='text-xl font-bold'>{abonnement.nom}</h2>
             <p>{abonnement.mensualite}€ | {abonnement.categorie}</p>
--- app/composants/NavBar.tsx
+++ app/composants/NavBar.tsx
@@ -2,11 +2,11 @@ import Link from "next/link";
 
 export default function NavBar() {
     return (
-        <header className='w-full h-1/6 flex justify-between items-center px-8 border-b-2 border-gray-500'>
+        <header className='w-full flex justify-between items-center px-8 border-b-2 rounded-xl border-gray-500'>
             <Link href='/dashboard'><img src='/logo.png' alt='Logo du site' className='w-[70px] md:w-[100px] h-[100px]' /></Link>
-            <Link href='/dashboard/ajouter' className='w-1/4 md:w-1/12'><img src='/add.svg' className='w-1/2' /></Link>
-            <Link href='/dashboard/analyse' className='w-1/4 md:w-1/12'><img src='/analyse.svg' className='w-1/2' /></Link>
-            <Link href='/profil' className='w-1/4 md:w-1/12'><img src='/profile.svg' className='w-1/2' /></Link>
+            <Link href='/dashboard/ajouter' className='w-1/4 md:w-1/12 font-bold text-lg'>Ajouter</Link>
+            <Link href='/dashboard/analyse' className='w-1/4 md:w-1/12 font-bold text-lg'>Analyse</Link>
+            <Link href='/profil' className='w-1/4 md:w-1/12 '><img src='/profile.svg' className='w-1/3' /></Link>
         </header>
     );
 }
