5d19104e0eaad10310b814f11de05dbbb8dae2ee
Deleplanque Matheo <matheo.deleplanque@gmail.com>
2024-12-29 00:30:45 +0100

    Quelques ameliorations

--- app/profil/layout.tsx
+++ app/profil/layout.tsx
@@ -6,7 +6,7 @@ export default function ProfilLayout({ children, }: Readonly<{ children: React.R
         <>
             <NavBar />
             <main className='w-full h-5/6 flex flex-col md:flex-row justify-center items-center'>
-                <nav className='w-10/12 md:w-1/12 h-1/6 md:h-3/4 flex md:flex-col items-center justify-around p-10 border-2 border-gray-800 rounded-xl '>
+                <nav className='w-10/12 md:w-1/12 h-1/6 md:h-3/4 flex md:flex-col items-center justify-around p-10 border-2 border-gray-800 '>
                     <Link href='/profil' className='w-10/12 flex flex-col items-center gap-2'>
                         <img src='/infos.svg' alt='Profil' className='w-1/6 md:w-auto' />
                         Informations
@@ -16,7 +16,7 @@ export default function ProfilLayout({ children, }: Readonly<{ children: React.R
                         Parametres
                     </Link>
                 </nav>
-                <section className='w-10/12 md:w-4/6 h-4/6 md:h-3/4 flex flex-col gap-10 p-10 border-2 border-gray-800 rounded-xl '>
+                <section className='w-10/12 md:w-4/6 h-4/6 md:h-3/4 flex flex-col gap-10 p-10 border-2 border-t-0 md:border-t-2 md:border-l-0 border-gray-800 '>
                     {children}
                 </section>
             </main>
